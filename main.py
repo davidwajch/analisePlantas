7feaf59162c3f5bba59a733f24c392140a2fbe8f
davidwajch <67844109+davidwajch@users.noreply.github.com>
2023-06-04 13:56:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ def processar_imagem(image, img: bool):
 
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    lower_color = np.array([10, 50, 50])
-    upper_color = np.array([30, 255, 255])
+    low_color = np.array([10, 50, 50])
+    up_color = np.array([30, 255, 255])
 
-    mask = cv2.inRange(hsv_image, lower_color, upper_color)
+    mask = cv2.inRange(hsv_image, low_color, up_color)
     kernel = np.ones((5, 5), np.uint8)
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
@@ -21,31 +21,31 @@ def processar_imagem(image, img: bool):
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     total_area = image.shape[0] * image.shape[1]
-    affected_area = sum(cv2.contourArea(contour) for contour in contours)
-    dead_percentage = round((affected_area / total_area) * 100, 2)
-    dead_tot += dead_percentage
+    area_afetada = sum(cv2.contourArea(contour) for contour in contours)
+    dead_porcent = round((area_afetada / total_area) * 100, 2)
+    dead_tot += dead_porcent
 
     if count % 20 == 1:
-        avg_dead_percentage = dead_tot / count
+        avg_dead_porcent = dead_tot / count
 
-        if avg_dead_percentage < 10:
-            print("A porcentagem de folha morta é baixa:", avg_dead_percentage, "%")
+        if avg_dead_porcent < 10:
+            print("A porcentagem de folha morta é baixa:", avg_dead_porcent, "%")
             print("Nenhuma ação imediata é necessária.")
-        elif avg_dead_percentage < 50:
-            print("A porcentagem de folha morta é moderada:", avg_dead_percentage, "%")
+        elif avg_dead_porcent < 50:
+            print("A porcentagem de folha morta é moderada:", avg_dead_porcent, "%")
             print("Recomenda-se realizar ações de controle e tratamento adequados.")
         else:
-            print("A porcentagem de folha morta é alta:", avg_dead_percentage, "%")
+            print("A porcentagem de folha morta é alta:", avg_dead_porcent, "%")
             print("Ações de controle e tratamento devem ser tomadas urgentemente.")
     elif img:
-        if dead_percentage < 10:
-            print("A porcentagem de folha morta é baixa:", dead_percentage, "%")
+        if dead_porcent < 10:
+            print("A porcentagem de folha morta é baixa:", dead_porcent, "%")
             print("Nenhuma ação imediata é necessária.")
-        elif dead_percentage < 50:
-            print("A porcentagem de folha morta é moderada:", dead_percentage, "%")
+        elif dead_porcent < 50:
+            print("A porcentagem de folha morta é moderada:", dead_porcent, "%")
             print("Recomenda-se realizar ações de controle e tratamento adequados.")
         else:
-            print("A porcentagem de folha morta é alta:", dead_percentage, "%")
+            print("A porcentagem de folha morta é alta:", dead_porcent, "%")
             print("Ações de controle e tratamento devem ser tomadas urgentemente.")
 
     cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
